1b694b086a7ebb7738bbce14025405dd0b7c2105
garywangop <garywangop@gmail.com>
2020-01-31 00:14:23 -0500

    Add different images to different rows

--- app/src/main/java/com/laioffer/matrix/EventAdapter.java
+++ app/src/main/java/com/laioffer/matrix/EventAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.List;
 
@@ -17,21 +18,11 @@ public class EventAdapter extends BaseAdapter {
         eventData = DataService.getEventData();
     }
 
-    /**
-     * list view lenght
-     */
     @Override
     public int getCount() {
         return eventData.size();
     }
 
-    /**
-     * Get the data item associated with the specified position in the data set.
-     *
-     * @param position Position of the item whose data we want within the adapter's
-     *                 data set.
-     * @return The data at the specified position.
-     */
     @Override
     public Event getItem(int position) {
         return eventData.get(position);
@@ -42,14 +33,6 @@ public class EventAdapter extends BaseAdapter {
         return position;
     }
 
-    /**
-     * @param position
-     * @param convertView the old view to reuse, if possible. Note: You should check that this view
-     *                    is non-null and of an appropriate type before using.
-     *                    If it is not possible to convert this view to display the correct data, this method can create a new view.
-     * @param parent the parent that this view will eventually be attached to
-     * @return Gets a View that displays in the listView with the data at the specified position in the data set.
-     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
@@ -57,6 +40,13 @@ public class EventAdapter extends BaseAdapter {
                     Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.event_item,
                     parent, false);
+        }
+
+        ImageView eventImage = (ImageView) convertView.findViewById(R.id.event_thumbnail);
+        if (position % 2 == 1) {
+            eventImage.setImageDrawable(context.getDrawable(R.drawable.event_thumbnail));
+        } else {
+            eventImage.setImageDrawable(context.getDrawable(R.drawable.lemon));
         }
 
         TextView eventTitle = (TextView) convertView.findViewById(
